ccd93689fbe97f83dc8994d38154a022b08dbcaf
shubhamshekhawat012-del <shubhamshekhawat012@gmail.com>
2026-08-06 15:26:33 +0530

    Add Docker deployment configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,10 @@ app.add_middleware(
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "http://127.0.0.1:5501",
-        "http://localhost:5501"
+        "http://localhost:5501",
+        "http://127.0.0.1:8080",
+        "http://localhost:8080",
+        
     ],
     allow_credentials=True,
     allow_methods=["*"],
